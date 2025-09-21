7f8d6efb2bbd0b52c2e2460c81b5a3f29c09d497
ahmedpasha00 <ahmedpasha@example.com>
2025-09-21 20:09:31 +0300

    SplashScreen

--- lib/bookia.dart
+++ lib/bookia.dart
@@ -1,1 +1,11 @@
+import 'package:bookia/feature/splash/splash_screen.dart';
+import 'package:flutter/material.dart';
 
+class Bookia extends StatelessWidget {
+  const Bookia({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(debugShowCheckedModeBanner: false, home: SplashScreen());
+  }
+}
---
+++ lib/feature/splash/splash_screen.dart
@@ -0,0 +1,47 @@
+import 'package:bookia/feature/welcome/presentation/ui/welcon_screen.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/cupertino.dart';
+
+class SplashScreen extends StatefulWidget {
+  const SplashScreen({super.key});
+
+  @override
+  State<SplashScreen> createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  @override
+  void initState() {
+    super.initState();
+    Future.delayed(const Duration(seconds: 5), () {
+      Navigator.pushReplacement(
+        context,
+        MaterialPageRoute(builder: (context) => WelconScreen()),
+      );
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [Image.asset("assets/images/logo splash.png")],
+            ),
+            Padding(
+              padding: const EdgeInsets.symmetric(vertical: 4),
+              child: Text(
+                "Order Your Book Now",
+                style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
